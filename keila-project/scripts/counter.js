e1f5b13e43f479cddf13917f77a85f5204e9d65f
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-04-28 19:37:12 -0300

    added new page

--- keila-project/scripts/counter.js
+++ keila-project/scripts/counter.js
@@ -1,25 +1,35 @@
 const counters = document.querySelectorAll(".count");
 const speed = 200;
 
+var isInViewport = function(elem) {
+  var distance = elem.getBoundingClientRect();
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
 // The code to start the animation is now wrapped in a function
-const startCounters = () => {
-  counters.forEach((counter) => {
-    const updateCount = () => {
-      const target = parseInt(+counter.getAttribute("data-target"));
-      const count = parseInt(+counter.innerText);
-      const increment = Math.trunc(target / speed);
-      if (count < target) {
-        counter.innerText = count + increment;
-        setTimeout(updateCount, 1);
-      } else {
-        count.innerText = target;
-      }
-    };
-    updateCount();
-  });
-}
+
 
 // On the first scroll in this window, call the function to start the counters
-window.addEventListener('scroll', startCounters, {
-  once: true
-});
+window.addEventListener('scroll', function(event) {
+  counters.forEach((counter) => {
+    if (isInViewport(counter)) {
+      const updateCount = () => {
+        const target = parseInt(+counter.getAttribute("data-target"));
+        const count = parseInt(+counter.innerText);
+        const increment = Math.trunc(target / speed);
+        if (count < target) {
+          counter.innerText = count + increment;
+          setTimeout(updateCount, 10);
+        } else {
+          count.innerText = target;
+        }
+      };
+      updateCount(); 
+  }
+  });
+}, false);
