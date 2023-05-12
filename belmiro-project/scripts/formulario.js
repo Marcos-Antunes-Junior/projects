3e7dae53e84a6647a3884825e1e205ab121fb911
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-05-12 18:02:50 -0300

    added code to form

--- belmiro-project/scripts/formulario.js
+++ belmiro-project/scripts/formulario.js
@@ -1,6 +1,13 @@
 function myFunction(){
         const valorNome = document.getElementById("nomecompleto").value;
+        if(valorNome == ""){
+        alert("Por favor, preencha o formulário antes de enviar.");
+        }else{
         alert(`Obrigado, ${valorNome}! Entraremos em contato o mais breve possível.`);
+        }
+        
         
   
  }
+
+ 
