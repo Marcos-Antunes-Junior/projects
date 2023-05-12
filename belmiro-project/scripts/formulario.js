f0bfb66eefd1c56a853634c9adeb1b3a951865e0
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-05-12 13:57:06 -0300

    .

---
+++ belmiro-project/scripts/formulario.js
@@ -0,0 +1,6 @@
+function myFunction(){
+        const valorNome = document.getElementById("nomecompleto").value;
+        alert(`Obrigado, ${valorNome}! Entraremos em contato o mais breve possível.`);
+        
+  
+ }
