9809a88a71b7b1e5cabf29553c5c12c114ef4a7e
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-05-17 13:49:38 -0300

    .

--- belmiro-project/scripts/button.js
+++ belmiro-project/scripts/button.js
@@ -5,8 +5,8 @@ const formulario = document.querySelector("form");
 function scrolldiv() {
     var elem = document.getElementById("header-button");
     elem.scrollIntoView({behavior: "smooth"});
-    formulario.style.animation = "glow 0.3s ease-in-out 10";
-    setInterval(function() {formulario.removeAttribute("style")}, 3000);
+    formulario.style.animation = "glow 0.3s ease-in-out 15";
+    setInterval(function() {formulario.removeAttribute("style")}, 6000);
     
 }
 
